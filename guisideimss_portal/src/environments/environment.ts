74a0913cf034a648773b13f4a8f2202db2d9ce82
Jaime Rodriguez <jaime.rodriguez@imss.gob.mx>
2025-10-13 09:51:42 -0600

    Se agrega funcionalidad para mostrar el preview del acuse

--- guisideimss_portal/src/environments/environment.ts
+++ guisideimss_portal/src/environments/environment.ts
@@ -3,7 +3,7 @@
     seguridadApiUrl: 'http://localhost:4109/mssideimss-seguridad',
     catalogosApiUrl: 'http://localhost:4110/mssideimss-catalogos',
     contadoresApiUrl: 'http://localhost:4111/mssideimss-contadores',
-    acusesApiUrl: 'http://localhost:4111/mssideimss-acuses',
+    acusesApiUrl: 'http://localhost:4112/mssideimss-acuses',
     documentosApiUrl: 'http://localhost:4113/mssideimss-documentos',
     firmaDigitalUrl: 'http://172.16.23.224',
 
