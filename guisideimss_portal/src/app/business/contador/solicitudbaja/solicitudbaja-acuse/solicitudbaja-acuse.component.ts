03db63ad281f592f0cc5ff8397e418a0b0081274
Jaime Rodriguez <jaime.rodriguez@imss.gob.mx>
2025-11-03 10:22:30 -0600

    Se agrega el inicio del acuse de solicitud de baja

---
+++ guisideimss_portal/src/app/business/contador/solicitudbaja/solicitudbaja-acuse/solicitudbaja-acuse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudbaja-acuse',
+  imports: [],
+  templateUrl: './solicitudbaja-acuse.component.html',
+  styleUrl: './solicitudbaja-acuse.component.css'
+})
+export class SolicitudbajaAcuseComponent {
+
+}
